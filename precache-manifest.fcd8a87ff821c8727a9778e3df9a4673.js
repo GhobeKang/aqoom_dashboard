b6db2cca0f5f9e9474e2540ff4493b32b8e842c5
ghobekang <dreamty88@gmail.com>
2020-09-24 11:14:08 +0900

    Updates

---
+++ precache-manifest.fcd8a87ff821c8727a9778e3df9a4673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97a579c15e93eb6de9615f151f6fdd1",
+    "url": "/aqoom_dashboard/index.html"
+  },
+  {
+    "revision": "abac590419f746b7f2aa",
+    "url": "/aqoom_dashboard/static/css/main.47cf30bd.chunk.css"
+  },
+  {
+    "revision": "12fec1ab4ad766184ccc",
+    "url": "/aqoom_dashboard/static/js/2.ff3b20de.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/aqoom_dashboard/static/js/2.ff3b20de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abac590419f746b7f2aa",
+    "url": "/aqoom_dashboard/static/js/main.3b7f69f0.chunk.js"
+  },
+  {
+    "revision": "1f20a29b264c9103ea9d",
+    "url": "/aqoom_dashboard/static/js/runtime-main.94b1bd5f.js"
+  }
+]);
